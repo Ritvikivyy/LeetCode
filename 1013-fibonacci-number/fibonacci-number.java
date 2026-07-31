894c0dcb3b5486f50851483d57b3427a544ad074
Ritvik Sharma <ritvik.sh7@gmail.com>
2026-07-31 12:22:41 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (52.93%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fib(int n) {
+        int a=0;
+        int b=1;
+        for(int i =1;i<n;i++){
+            int c =a+b;
+            a=b;
+            b=c;
+        }
+        if(n==0){
+            return 0;
+        }
+        else{
+            return b;
+        }
+    }
+}
